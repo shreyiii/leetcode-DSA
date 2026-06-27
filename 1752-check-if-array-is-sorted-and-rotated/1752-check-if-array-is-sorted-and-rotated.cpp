bf2ad806550d726de29c48e07740b3dd5380f4f5
SHREY SRIVASTAVA <shreysrivastavaaks_ds24@its.edu.in>
2026-06-28 00:08:18 +0530

    Time: 0 ms (100%), Space: 11.3 MB (19.33%) - LeetHub

---
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int n=nums.size();
+        int c=0 ;
+        for(int i=0;i<n;i++){
+            if (nums[i]>nums[(i+1)%n]){
+                c++;
+            }
+        }                          
+        return c<=1;
+    }
+};
