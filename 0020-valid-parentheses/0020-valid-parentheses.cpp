2807f375e1898f46ae0c850e697f49a95fbd8030
SHREY SRIVASTAVA <shreysrivastavaaks_ds24@its.edu.in>
2026-07-01 13:32:35 +0530

    Time: 0 ms (100%), Space: 9.3 MB (11.7%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValid(string s) {
+       unordered_map<char, char> mp = {
+            {')', '('},
+            {'}', '{'},
+            {']', '['}
+        };
+
+        stack<char> st;
+
+        for (char ch : s) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            } else {
+                if (st.empty() || st.top() != mp[ch])
+                    return false;
+                st.pop();
+            }
+        }
+
+        return st.empty();
+    }
+};
